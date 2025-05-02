f50eeef373f40843df63345a012ed31c3c9ca1eb
Lizaveta-Moisa <sliza1882@gmail.com>
2025-05-02 10:25:15 +0300

    refactor class Main (add sum, a)

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int b = 5;
+        int a = 0;
+        int sum = 0;
 
         for (int i = 0; i <= b; i++) {
             if (i >= b) {
-                System.out.println("i равно = " + i);
+                System.out.println("sum равно = " + sum);
+            }
+            else {
+                sum += i;
             }
         }
     }
