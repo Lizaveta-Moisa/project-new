f2f71dad58a8b2fcb8c09a46f829e34c5b047fa4
Lizavete-Moisa <sliza1882@gmail.com>
2025-05-02 10:08:52 +0300

    refactor class Main

--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int b = 5;
 
-        Hypot hypot = new Hypot();
-        hypot.heigthMM = 3;
-        hypot.radiusMM = 1;
-        double volumeOfHypot = hypot.methodCalculate();
-        System.out.println("Объём цилиндра: " + volumeOfHypot);
-
-        Cube cube1 = new Cube();
-        cube1.sideMM = 3;
-        int volumeOfCube = cube1.calculateVolume();
-        System.out.println("Объём куба: " + volumeOfCube);
+        for (int i = 0; i <= b; i++) {
+            if (i >= b) {
+                System.out.println("i равно = " + i);
+            }
+        }
     }
 }
